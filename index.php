c96f1a67bf0e26aaaf5678400e600abfb155d4f7
William Armsworthy <rise.at.night.help@gmail.com>
2017-02-23 16:19:35 -0500

    Adding some more stuff

--- index.php
+++ index.php
@@ -12,19 +12,23 @@
     <link rel="stylesheet" href="./Content/app.css">
 </head>
 <body>
-<div class="container">
-    <div class="login-container">
-        <div id="output"></div>
-        <div class="avatar"></div>
-        <div class="form-box">
-            <form action="" method="">
-                <input name="user" type="text" placeholder="username">
-                <input type="password" placeholder="password">
-                <button class="btn btn-info btn-block login" type="submit">Login</button>
+<a href="#" data-toggle="modal" data-target="#login-modal">Login</a>
+
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog">
+        <div class="loginmodal-container">
+            <h1>Login to Your Account</h1><br>
+            <form>
+                <input type="text" name="user" placeholder="Username">
+                <input type="password" name="pass" placeholder="Password">
+                <input type="submit" name="login" class="login loginmodal-submit" value="Login">
             </form>
+
+            <div class="login-help">
+                <a href="#">Register</a> - <a href="#">Forgot Password</a>
+            </div>
         </div>
     </div>
-
 </div>
 </body>
 </html>
