d745c40c0d9e1ce78a4f893c572b74e2213d7930
William Armsworthy <rise.at.night.help@gmail.com>
2017-02-23 16:20:42 -0500

    Adding some more stuff

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
         <div class="loginmodal-container">
             <h1>Login to Your Account</h1><br>
             <form>
-                <input type="text" name="user" placeholder="Username">
-                <input type="password" name="pass" placeholder="Password">
+                <input type="text" name="user" placeholder="Enter Your Username">
+                <input type="password" name="pass" placeholder="Enter Your Password">
                 <input type="submit" name="login" class="login loginmodal-submit" value="Login">
             </form>
 
