e5957b92333c2eef73e8868e523b9f4e23d77291
William Armsworthy <rise.at.night.help@gmail.com>
2017-02-23 16:25:27 -0500

    Adding some more stuff

--- index.php
+++ index.php
@@ -8,23 +8,38 @@
     <link rel="stylesheet" href="./Scripts/lib/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./Scripts/lib/bootstrap/dist/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="./Scripts/lib/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="mycss.css">
+    <script src="http://mymaplist.com/js/vendor/TweenLite.min.js"></script>
     <link rel="stylesheet" href="./Content/app.css">
 </head>
 <body>
 <div class="container">
-    <div class="login-container">
-        <div id="output"></div>
-        <div class="avatar"></div>
-        <div class="form-box">
-            <form action="" method="">
-                <input name="user" type="text" placeholder="username">
-                <input type="password" placeholder="password">
-                <button class="btn btn-info btn-block login" type="submit">Login</button>
-            </form>
+    <div class="row vertical-offset-100">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Please sign in</h3>
+                </div>
+                <div class="panel-body">
+                    <form accept-charset="UTF-8" role="form">
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="E-mail" name="email" type="text">
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                            </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input name="remember" type="checkbox" value="Remember Me"> Remember Me
+                                </label>
+                            </div>
+                            <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-
 </div>
 </body>
 </html>
